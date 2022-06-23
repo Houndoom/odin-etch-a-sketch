5203408908ec5cfc186a73b898d9f8477770bea9
Houndoom <therealhoundoom@gmail.com>
2022-06-23 18:10:16 +0800

    Change to random colors that darken after every pass

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/* Global variable */
+
+var colorTimesArray = Array(16*16);
+var noOfSquares = 16;
+
 /* Create grid */
 
 function removeSquares() {
@@ -10,18 +15,21 @@ function createGrid(noOfSquares) {
   for (let i = 1; i <= noOfSquares*noOfSquares; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
+    square.setAttribute('data-square-id',i);
     board.appendChild(square);
   }
 
   board.style.gridTemplateColumns = `repeat(${noOfSquares},auto)`;
+  draw();
 }
+
 /* Change number of squares button */
 
 const noOfSquaresButton = document.querySelector('#no-of-squares');
 noOfSquaresButton.addEventListener('click',changeGrid)
 
 function changeGrid() {
-  let noOfSquares = parseInt(prompt('Please enter the number of squares per side of the grid. Please enter a number from 1 to 100.'));
+  noOfSquares = parseInt(prompt('Please enter the number of squares per side of the grid. Please enter a number from 1 to 100.'));
   console.log(noOfSquares);
   if (isNaN(noOfSquares)) {
     alert('Please enter a number');
@@ -37,3 +45,52 @@ function changeGrid() {
     return;
   }
 }
+
+/* Clear board button */
+
+const clearBoardButton = document.querySelector('#clear-board');
+clearBoardButton.addEventListener('click',clearBoard)
+
+function clearBoard() {
+  removeSquares();
+  createGrid(noOfSquares);
+}
+
+/* Color squares */
+function draw() {
+  const allSquares = document.querySelectorAll('.square');
+  allSquares.forEach(e => e.addEventListener('mouseenter',colorSquare));
+};
+
+function colorSquare() {
+  if (!this.style.backgroundColor) {
+    this.style.backgroundColor = getRandomColor();
+    this.style.borderColor = this.style.backgroundColor;
+    colorTimesArray[this.getAttribute('data-square-id')] = 10;
+  } else {
+    let colorTimes = colorTimesArray[this.getAttribute('data-square-id')];
+      if (colorTimes > 0) {
+        colorTimes--;
+        let colorRegex = this.style.backgroundColor.split(/[\(,\,\), ]+/);
+        newColor = `rgb(${adjustRGB(...colorRegex.slice(1,4),colorTimes)})`;
+        this.style.backgroundColor = newColor;
+        this.style.borderColor = newColor;
+        colorTimesArray[this.getAttribute('data-square-id')] = colorTimes;
+      };
+  }
+};
+
+function adjustRGB(r,g,b,colorTimes) {
+  let newr = r*colorTimes/(colorTimes + 1);
+  let newg = g*colorTimes/(colorTimes + 1);
+  let newb = b*colorTimes/(colorTimes + 1);
+  return [newr,newg,newb];
+}
+
+function getRandomColor() {
+  return `rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`;
+}
+
+/* Create initial grid */
+
+createGrid(noOfSquares);
